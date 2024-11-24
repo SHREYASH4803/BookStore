b6cd576a4a91aa2b15183a62b9324a79bde8d94c
shreyash4803 <shreyash4803@gmail.com>
2024-11-24 19:13:19 +0530

    .

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get('https://book-store-backend.vercel.app')
+      .get('https://book-store-backend.vercel.app/books')
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
