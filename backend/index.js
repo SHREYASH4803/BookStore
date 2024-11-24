62f24880c4ec0901b0459412421e9799174bc28a
shreyash4803 <shreyash4803@gmail.com>
2024-11-24 17:59:46 +0530

    deployment

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,28 @@
 import express from 'express';
-import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
-import booksRoute from './routes/booksRoute.js';
 import cors from 'cors';
+
+import { PORT, mongoDBURL } from './config.js';
+import booksRoute from './routes/booksRoute.js';
 
 const app = express();
 
 
 app.use(express.json());
-
-
 app.use(cors());
 
 
-app.get('/', (request, response) => {
-  console.log(request);
-  return response.status(234).send('check');
+app.get('/', (req, res) => {
+  console.log(req);
+  return res.status(234).send('check');
 });
 
 app.use('/books', booksRoute);
 
+
 mongoose
   .connect(mongoDBURL)
-  .then(() => {
-    console.log('connected ');
-    app.listen(PORT, () => {
-      console.log(`App is listening to port: ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.log(error));
+
+export default app; 
