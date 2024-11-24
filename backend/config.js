bf2b6d0e0ae1aef233c2146deea7d04f69ada254
shreyash4803 <shreyash4803@gmail.com>
2024-11-24 18:36:41 +0530

    .

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://shreyash4803:shreyash4803@project0.ptyfjkk.mongodb.net/BookStore?retryWrites=true&w=majority';
+  'mongodb+srv://shreyash4803:shreyash4803@bookstore.ptyfjkk.mongodb.net/?retryWrites=true&w=majority&appName=BookStore';
 
